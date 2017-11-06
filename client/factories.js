d4cae2b23570f9b04d39f8bf2478ae731106f695
“ddmerci” <31666738+ddmerci@users.noreply.github.com>
2017-11-06 15:47:37 -0500

    multichoice.js & color-game.js text moved to controllers.js + minor typo corrections to other files

--- client/factories.js
+++ client/factories.js
@@ -1,4 +1,4 @@
-angular.module('codecc.factories', [])
+angular.module('codecc.factories', ['ngResource'])
 .factory('Post', ['$resource', function($resource) {
     return $resource('/api/posts/:id', { id: '@id' }, {
         update: {
@@ -7,31 +7,31 @@ angular.module('codecc.factories', [])
     });
 }])
 .factory('User', ['$resource', function($resource) {
-    return $resource('/api/users/:id'), { id: '@id' }, {
+    return $resource('/api/users/:id', { id: '@id' }, {
         update: {
             method: 'PUT'
         }
-    }
+    });
 }])
 .factory('Reply', ['$resource', function($resource) {
-    return $resource('/api/replies/:id'), { id: '@id' }, {
+    return $resource('/api/replies/:id', { id: '@id' }, {
         update: {
             method: 'PUT'
         }
-    }
+    });
 }])
 .factory('Bootcamp', ['$resource', function($resource){
-    return $resource('/api/bootcamps'), { id: '@id' }, {
+    return $resource('/api/bootcamps', { id: '@id' }, {
         update: {
             method: 'PUT'
         }
-    }
+    });
 }])
 .factory('Review', ['$resource', function($resource){
-    return $resource('/api/review/:id'), { id: '@id' }, {
+    return $resource('/api/review/:id', { id: '@id' }, {
         update: {
             method: 'PUT'
         }
-    }
-}])
+    });
+}]);
 
